7d30427acab975c7e27bcd419bcff7b95a61f6e2
lenomirei <lenomirei@163.com>
2021-03-22 22:21:37 +0800

    初步添加朝向发射

--- domino/Assets/ShootBallCamera.cs
+++ domino/Assets/ShootBallCamera.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class ShootBallCamera : MonoBehaviour
 {
     Transform main_camera_;
-    float camera_speed_;
+    float camera_speed_ = 10.0f;
     Vector3 camera_rot_;
     public GameObject sphere_;
+    public float shoot_force_ = 200.0f;
     // Start is called before the first frame update
     void Start()
     {
         main_camera_ = Camera.main.transform;
-        camera_speed_ = 10.0f;
+        camera_rot_ = new Vector3(0, 0, 0);
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -41,7 +42,6 @@ public class ShootBallCamera : MonoBehaviour
         float rh = Input.GetAxis("Mouse X");
         float rv = Input.GetAxis("Mouse Y");
 
-        camera_rot_ = main_camera_.eulerAngles;
         camera_rot_.x -= rv;
         camera_rot_.y += rh;
         main_camera_.eulerAngles = camera_rot_;
@@ -49,8 +49,8 @@ public class ShootBallCamera : MonoBehaviour
 
     void ShootSphere() {
         Vector3 cam_pos = main_camera_.transform.localPosition;
-        cam_pos.z += 2;
         GameObject sphere = GameObject.Instantiate(sphere_, cam_pos, main_camera_.transform.rotation);
-        sphere.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 200.0f), ForceMode.Acceleration);
+        float sum = Mathf.Abs(camera_rot_.x) + Mathf.Abs(camera_rot_.y);
+        sphere.GetComponent<Rigidbody>().AddForce(new Vector3(camera_rot_.y / sum * shoot_force_, -camera_rot_.x / sum * shoot_force_, shoot_force_), ForceMode.Acceleration);
     }
 }
