7d46cb52b686e1e71362d3a5f95a7598cd953b86
lenomirei <lenomirei@163.com>
2021-04-08 22:49:01 +0800

    更新时钟

--- clock/Assets/Scripts/Indicator.cs
+++ clock/Assets/Scripts/Indicator.cs
@@ -9,21 +9,19 @@ public class Indicator : MonoBehaviour
     public Transform minute_indicator_;
     public Transform hour_indicator_;
     protected const float drgree_per_hour_ = 30f;
+    protected const float drgree_per_ms_ = 6f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private void Awake()
-    {
-        DateTime time = DateTime.Now;
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        DateTime time = DateTime.Now;
+        hour_indicator_.localRotation = Quaternion.Euler(new Vector3(0, time.Hour * drgree_per_hour_, 0));
+        minute_indicator_.localRotation = Quaternion.Euler(new Vector3(0, time.Minute * drgree_per_ms_, 0));
+        second_indicator_.localRotation = Quaternion.Euler(new Vector3(0, time.Second * drgree_per_ms_, 0));
     }
 }
