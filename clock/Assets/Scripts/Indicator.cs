55b288c6e658fba519a9e36972265a615577e43c
lenomeng <lenomeng@tencent.com>
2021-04-08 20:22:44 +0800

    Add clock project

---
+++ clock/Assets/Scripts/Indicator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicator : MonoBehaviour
+{
+    public Transform second_indicator_;
+    public Transform minute_indicator_;
+    public Transform hour_indicator_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
