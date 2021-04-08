d9640932c91f1aa1d3c73c25fd94667ab8e949cc
lenomeng <lenomeng@tencent.com>
2021-04-08 21:02:09 +0800

    update

--- clock/Assets/Scripts/Indicator.cs
+++ clock/Assets/Scripts/Indicator.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Indicator : MonoBehaviour
 {
     public Transform second_indicator_;
     public Transform minute_indicator_;
     public Transform hour_indicator_;
+    protected const float drgree_per_hour_ = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,8 @@ public class Indicator : MonoBehaviour
 
     private void Awake()
     {
-        
+        DateTime time = DateTime.Now;
+
     }
 
     // Update is called once per frame
